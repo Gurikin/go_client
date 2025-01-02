d67812f704fa59b5f05e560ad6ad8f68ceb3fc05
harry <gurikin@yandex.ru>
2025-01-03 01:59:57 +0300

    another one try to use quiche

--- gs/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION)?;
-    config.set_application_protos(&[b"example-proto"]);
-    // config.set_initial_max_streams_bidi(val::MAX_STREAMS_BIDI);
-    let conn = quiche::accept(&scid, None, local, peer, &mut config)?;
-}
